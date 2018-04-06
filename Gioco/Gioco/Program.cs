8f56b4119cce28104e0e5ea1b905371905952972
FrancescoRossi <francesco.rossi@scai-consulting.grupposcai.it>
2018-04-06 14:30:29 +0200

    Started

---
+++ Gioco/Gioco/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gioco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
